57afaa86c9286e7f7b73c50c67c20cb9123b3f6c
mgthihazaw <mgthihazaw777@gmail.com>
2019-05-27 16:00:02 +0630

    add exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,12 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Database\QueryException;
 
 class Handler extends ExceptionHandler
 {
@@ -34,6 +40,8 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        
+       
         parent::report($exception);
     }
 
@@ -44,8 +52,38 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Exception $e)
     {
-        return parent::render($request, $exception);
+        // if ($e instanceof ValidationException) {
+        //     return response(['error' =>"mm"],404);
+        // }
+        if($e instanceof NotFoundHttpException){
+            return response(['error' =>"URL does not exit {base_classname($e)}","code"=>404],404);
+        }
+        if($e instanceof ModelNotFoundException){
+            $model=class_basename($e->getModel());
+            return response(['error' =>"Does not exit any {$model} with this specified identificator","code"=>404],404);
+        }
+        if($e instanceof AuthenticationException){
+            return response(['error' =>"UnAuthenticated User","code"=>403],403);
+        }
+        if($e instanceof AuthorizationException){
+            return response(['error' =>"Unauthorized User","code"=>403],403);
+        }
+        if($e instanceof MethodNotAllowedHttpException){
+            return response(['error' =>"This specified method for the request is invalid","code"=>$e->getStatusCode()],$e->getStatusCode());
+        }
+        if($e instanceof HttpException){
+            return response(['error' =>$e->getMessage(),"code"=>$e->getStatusCode()],$e->getStatusCode());
+        }
+        if($e instanceof QueryException){
+            // dd($e);
+            $errorCode =$e->errorInfo[2];
+           return  $code =$e->errorInfo;
+            return response(['error' =>$errorCode,"code"=>409],409);
+        }
+
+        return parent::render($request, $e);
     }
+    
 }
