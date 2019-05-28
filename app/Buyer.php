ca5740e932f5f6414601f9ddae94e8a2f3ee787f
mgthihazaw <mgthihazaw777@gmail.com>
2019-05-28 10:00:34 +0630

    add implicit model binding and globalscope

--- app/Buyer.php
+++ app/Buyer.php
@@ -4,10 +4,14 @@ namespace App;
 
 // use App\Transaction;
 use Illuminate\Database\Eloquent\Model;
+use App\Scopes\BuyerScope;
 
 class Buyer extends User
 {
-    
+    protected static function boot(){
+        parent::boot();
+        static::addGlobalScope(new BuyerScope);
+    }
     public function transactions()
     {
         return $this->hasMany(Transaction::class);
--- app/Seller.php
+++ app/Seller.php
@@ -4,9 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\User;
+use App\Scopes\SellerScope;
 
 class Seller extends User
 {
+    protected static function boot(){
+        parent::boot();
+        static::addGlobalScope(new SellerScope);
+
+    }
     public function products()
     {
         return $this->hasMany(Product::class);
